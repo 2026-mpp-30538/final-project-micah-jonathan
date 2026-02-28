90f550a0f6db330eebc32b9e7edb91ddbac1d8c2
Micah Slaughter <micahs@uchicago.edu>
2026-02-27 18:40:31 -0600

    adding health data files and processing code

---
+++ code/preprocessing-Micah.py
@@ -0,0 +1,57 @@
+import geopandas as gpd
+import pandas as pd
+from pathlib import Path
+from shapely import wkt
+
+script_dir = Path(__file__).parent
+
+# Creating single file of emissions related cancers by county for 2016-2020  
+files = [
+    "lung-bronch_cancer",
+    "breast_cancer",
+    "colorectal_cancer",
+    "kidney_cancer",
+    "bladder_cancer"]
+
+combined_df = pd.DataFrame()
+
+for file in files:
+    raw_cancer = script_dir / f"../data/raw-data/{file}.csv"
+    output_cancer = script_dir / f"../data/derived-data/{file}_filtered.csv"
+    
+    df = pd.read_csv(raw_cancer)
+
+    cancer_col = df.columns[6]
+
+    df = df[["CountyFIPS", "County", cancer_col]]
+    
+    df = df.rename(columns={cancer_col: file})
+    
+    if combined_df.empty:
+        combined_df = df
+    else:
+        combined_df = pd.merge(combined_df, df, on=["CountyFIPS", "County"], how="outer")
+
+final_output = script_dir / "../data/derived-data/combined_cancer_by_county.csv"
+
+combined_df.to_csv(final_output, index=False)
+
+# Cleaning crude prevalence of COPD by county for 2023 
+raw_copd = script_dir / '../data/raw-data/crude_COPD.csv'
+output_copd = script_dir / '../data/derived-data/COPD_filtered.csv'
+
+copd_df = pd.read_csv(raw_copd)
+
+copd_filtered = copd_df[["CountyFIPS", "County", "Year", "Value"]]
+
+copd_filtered.to_csv(output_copd, index=False)
+
+# Cleaning crude prevalance of asthma by county for 2023
+raw_asthma = script_dir / '../data/raw-data/crude_asthma.csv'
+output_asthma = script_dir / '../data/derived-data/asthma_filtered.csv'
+
+asthma_df = pd.read_csv(raw_asthma)
+
+asthma_filtered = asthma_df[["CountyFIPS", "County", "Year", "Value"]]
+
+asthma_filtered.to_csv(output_asthma, index=False)
